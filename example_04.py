e2ad286fbcc6856ec8024cc1e1150799ef93d4c3
diyorbekqodirboyev <131951584+diyorbekqodirboyev@users.noreply.github.com>
2023-04-27 14:07:59 +0500

    Create example_04.py

---
+++ example_04.py
@@ -0,0 +1,17 @@
+cars = ['tayota', 'bmw', 'honda', 'nissan', 'hyundai', 'kia', 'audi', 'mersedes']
+
+# ro'yxatga murojaat qilish
+print(cars[0]) #birinchi element, tayota
+print(cars[1]) #ikkinchi element, bmw
+print(cars[2]) #uchinchi element, honda
+print(cars[3]) #to'rtinchi element, nissan
+print(cars[4]) #beshinchi element, hyundai
+print(cars[5]) #oltinchi element, kia
+print(cars[6]) #yettinchi element, audi
+print(cars[7]) #sakkizinchi element, mersedes
+
+# len() funksiyasi yordamida ro'yixatdagi elementlar sonini anqlash
+print(len(cars))
+
+# len() funksiyasi yordamida ro'yixatdagi element belgilari sonini anqlash
+print(len(cars[0]))
